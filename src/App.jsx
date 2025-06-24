cf98351f1c987fd7a73bdb72b33eac1764cec693
FernandoLabastida <labastidafernando@comunidad.unam.mx>
2025-06-24 17:05:38 -0600

    Uso de props para reutilizar componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import Card from './components/Card'
 import './App.css'
 
 function App() {
 
   const items = ['React', 'JavaScript', 'Vite']
 
+  const info = [
+    { title: 'Teresa', description: 'Capibara' },
+    { title: 'Gatita', description: 'Bonita'},
+    { title: 'Ilia', description: 'Topuria'}
+  ]
+
   return  (
     <div>
+      <h1>Hola Mundo!</h1>
+
+      {
+        info.map((item, index) => (
+          <Card 
+            key={index}
+            title={item.title}
+            description={item.description}
+          />
+        ))
+      }
+      
       <ul>
         {
           items.map((item, index) => (
